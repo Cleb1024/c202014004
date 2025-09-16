41eccfa323d82aa56419c4b546dccca449bcd907
Kim, Inyoung <kiy9398@gmail.com>
2025-09-17 02:35:24 +0900

    five

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
   public static void main(String[] args) {
+<<<<<<< HEAD
     System.out.println("홍길동"); // 이름 그대로
     World.run();
     Hello.run();                 // ← 이 줄 추가!
+=======
+    System.out.println("홍길동"); // ← 학생A 이름 그대로
+    World.run();
+    World.run2();                 // ← 새로 추가
+>>>>>>> 57e8a46 (five)
   }
 }
 
---
+++ src/World.java
@@ -0,0 +1,9 @@
+public class World {
+  public static void run() {
+    System.out.println("202014004"); // ← 본인 학번
+  }
+
+  public static void run2() {
+    System.out.println("hello world");
+  }
+}
