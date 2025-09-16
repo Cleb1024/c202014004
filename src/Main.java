5a7fed79b566fdeb3e4d9e5ac36f432e073bb91c
Kim, Inyoung <kiy9398@gmail.com>
2025-09-17 02:12:12 +0900

    three

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+// src/Main.java
 public class Main {
   public static void main(String[] args) {
-    System.out.println("김인영");  // ← 네 이름으로 수정
+    System.out.println("홍길동"); // ← 학생A가 쓴 이름 유지
+    World.run();                 // ← 호출 추가
   }
 }
+
